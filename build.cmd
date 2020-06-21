befb7b846d8c19e9b76da9423fc8f2f9e1755683
mesota <mesota72@gmail.com>
2020-06-21 19:06:47 +0200

    AlphaGear2 V6.9.1 initial commit

---
+++ build.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set name=AlphaGear_6.9.1.zip
+
+rem -----------------
+
+mkdir build 2>nul
+del build\%name% 2>nul
+"%PROGRAMFILES%\7-Zip\7z.exe" a -r build\%name% Alphagear\*.* 
+endlocal
---
+++ deploy.cmd
@@ -0,0 +1,2 @@
+@echo off
+robocopy AlphaGear "%USERPROFILE%\Documents\Elder Scrolls Online\live\AddOns\AlphaGear" /MIR
